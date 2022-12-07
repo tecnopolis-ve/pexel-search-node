e6a9b5518a1c242339a1785ec100b82a43a56409
Manuel <manuel_se@live.com>
2022-12-07 17:13:01 +0100

    add: minor test for coverage

--- tests/test.js
+++ tests/test.js
@@ -6,6 +6,18 @@ const api = supertest(server);
 describe("Api endpoint", () => {
     beforeEach(() => jest.clearAllMocks());
 
+    it("[KO] perform a call to an non existing resource", async () => {
+        const res = await api.get("/foo").send();
+        expect(res.statusCode).toEqual(404);
+    });
+
+    it("[OK] perform a basic server call", async () => {
+        const res = await api.get("/api/V1").send();
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toBeDefined();
+        expect(res.text).toBe('Alive!');
+    });
+
     it("[KO] perform a search without query param", async () => {
         const res = await api.get("/api/V1/search").send();
         expect(res.statusCode).toEqual(400);
@@ -16,8 +28,7 @@ describe("Api endpoint", () => {
         expect(res.statusCode).toEqual(200);
         expect(res.body.page).toEqual(1);
         expect(res.body.per_page).toEqual(30);
-        expect(Array.isArray(res.body.photos)).toBeTruthy()
-        expect(res.body.photos.length).toBeGreaterThan(0)
-
+        expect(Array.isArray(res.body.photos)).toBeTruthy();
+        expect(res.body.photos.length).toBeGreaterThan(0);
     });
 });
